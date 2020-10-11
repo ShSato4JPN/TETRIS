5cfdfaf060adf23df58d0c0d12f0aae525f92cc6
ShSato4 <ShSato4.private.1221@gmail.com>
2020-10-11 17:35:03 +0900

    ミノのスーパークラスの枠は完成。 テストでMinoJクラスも作成。

--- js/Mino.js
+++ js/Mino.js
@@ -2,33 +2,74 @@
   *   ミノに関する情報を管理するクラス
  **/
 class Mino {
-
-    constructor() {
+    constructor( color ) {
         // メンバ定義
-        init();
+        init( color );
         // ミノ情報を作成
         createMinoData();
     }
-
-    init() {
+    // メンバを定義
+    init( color ) {
         // 0 ~ 3 毎に45度回転する
         this.minoState = 0;
-        this.minoData  =
+        this.minoColor = color; // ミノの色
+        this.minoData  = undefined; // ミノの構造(2次元配列)
     }
-
     // サブクラスでOrverrideする
     createMinoData() {}
-
+    // ミノのステータスを取得
     get minoState() {
         return this.minoState;
     }
-
+    // ミノのステータスを設定
     set minoState( state ) {
         this.minoState = state;
     }
-
-    getMino() {
-
+    // ミノの色を取得
+    getMinoColor() {
+        return this.minoData[this.minoState][0];
     }
+    // ミノの現在の構成を取得
+    getMino() {
+        return this.minoData[this.minoState][1];
+    }
+}
 
+/** Class MinoJ
+  *   J型のミノ
+ **/
+class MinoJ extends Mino {
+    constructor() {
+        super( "#3399FF" );
+    }
+    // Override
+    createMinoData() {
+        let subArray0 = [
+            [1, 0, 0],
+            [1, 1, 1],
+            [0, 0, 0]
+        ];
+        let subArray1 = [
+            [0, 1, 1],
+            [0, 1, 0],
+            [0, 1, 0]
+        ];
+        let subArray2 = [
+            [0, 0, 0],
+            [1, 1, 1],
+            [0, 0, 1]
+        ];
+        let subArray3 = [
+            [0, 1, 0],
+            [0, 1, 0],
+            [1, 1, 0]
+        ];
+
+        this.this.minoData = [
+            [this.color, subArray0],
+            [this.color, subArray1],
+            [this.color, subArray2],
+            [this.color, subArray3],
+        ];
+    }
 }
